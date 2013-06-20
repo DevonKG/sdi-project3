54547668c6d41f2d5b5acca71bc9efd82f818685
Devon Gildark <devonkg@fullsail.edu>
2013-06-19 20:26:59 -0700

    Commite 3

--- DKG_Project3R.js
+++ DKG_Project3R.js
@@ -4,17 +4,31 @@
 //Project 3
 // 1306
 
-var ItemUseHP = function(HPotionUse) {
-    j2.ItemTotal[0] = j2.ItemTotal[0] - HPotionUse;
-    return j2.ItemTotal[0]
-    
-}
+//json Item
+var jsonItems = '[{"Type": "Health Potion",	"Value": 200,	"Total":10},{"Type": "Magic Potion","Value": 50,"Total": 10}]';
 
-var ItemUseMP = function(MPotionUse) {
-    j2.ItemTotal[1] = j2.ItemTotal[1] - MPotionUse;
-    return j2.ItemTotal[1]
-    
-}
+var jsonHeroes = '[{"Name": "Oddler","Life": 300,"MaxLife" : 300, "Magic": 150,"Strength": 250,"Defense": 200},{"Name": "Mazy","Life": 200,"LifeMax" : 200,"Magic": 300,"Strength": 120,"Defense": 175}]';
 
-var Odd = j3.HeroStats[0];
-var Maz = j3.HeroStats[1]
+
+var jsonEnemies = '[{"Name": "Bat","Life": 75,"Magic": 0,"Strength": 35,"Defense": 150},{"Name": "Ooze","Life": 125,"Magic": 0, "Strength": 30,"Defense": 200}, {"Name": "Ogre","Life": 350,"Magic": 0,"Strength": 300,"Defense": 100}]';
+//End json
+
+var objItems = JSON.parse(jsonItems);
+var objHeroes = JSON.parse(jsonHeroes);
+var objEnemies = JSON.parse(jsonEnemies);
+
+var Oddler = objHeroes[0];
+var Mazy = objHeroes[1];
+
+var HealthPotion = objItems[0];
+var ManaPotion = objItems[1];
+
+var Bat = objEnemies[0];
+var Ooze = objEnemies[1];
+var Ogre = objEnemies[2];
+
+var OddlerLifeThreshold = Oddler.Life *0.9;
+var MazyLifeThreshold = Mazy.Life * 0.5;
+var mazyManaThreshold = Mazy.Magic * 0.1;
+
+var healAmount = (parseInt(Math.random()*50)+25);
--- Json.js
+++ Json.js
@@ -1,50 +1,57 @@
-var json = {
-    "Items" : [
-      {  HealthPotion: +200} ,
-      {  MagicPotion: +50}
-    ] }
-    
-var j2 = {
-    "ItemTotal" : [
-    { HealthPotion: 10},
-    { MagicPotion: 10}
-    ]}
+var jsonItems = {
+	"Items" : [
+	{
+		Type: "HealthPotion",
+		Value: 200,
+		Total:10
+	},
+	{
+	  	Type: "MagicPotion",
+		Value: 50,
+		Total: 10
+	}]
+};
 
-var j3 = {
+var jsonHeroes = {
     "HeroStats" : [
     {// Oddler
+		Name: "Oddler",
         Life: 300,
         Magic: 150,
         Strength: 250,
         Defense: 200
         },
     {// Mazy
+		Name: "Mazy",
         Life: 200,
         Magic: 300,
         Strength: 120,
         Defense: 175
         }        
-    ]}
+    ]};
 
-var j4 = {
+var jsonEnemies = {
     "EnemyStats" : [
     {//Bat
+		Name: "Bat",
         Life: 75,
         Magic: 0,
         Strength: 35,
         Defence: 150
         },
     {// Ooze
+		Name: "Ooze",
         Life: 125,
         Magic: 0,
         Stength: 30,
         Defence: 200
         },
-    {// Oger
+    {// Ogre
+		Name: "Ogre",
         Life: 350,
         Magic: 0,
         Strength: 300,
         Defence: 100
-        }
+	}
     ]
-}
+};
